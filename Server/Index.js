bb0439ee4da94a9178f383fc1abf8a6d129fcb30
piyush-200 <piyushadhikari740@gmail.com>
2025-11-29 23:48:13 +0530

    Update Index.js

--- Server/Index.js
+++ Server/Index.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin: "const PORT = process.env.PORT || 4000",
+		origin: "https://client-server-99.onrender.com",
 		credentials: true,
 	})
 );
